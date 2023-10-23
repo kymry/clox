a67b16e81862fda73f2d9d1861f072433e6d3ce3
Kymry Burwell <burkymry@amazon.com>
2023-10-23 17:25:43 +0900

    add string support to the parser, compiler and interpreter

--- object.h
+++ object.h
@@ -6,12 +6,18 @@
 
 #define OBJ_TYPE(value)        (AS_OBJ(value)->type)
 
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+
 typedef enum {
   OBJ_STRING,
 } ObjType;
 
 struct Obj {
   ObjType type;
+  struct Obj* next;
 };
 
 struct ObjString {
@@ -20,4 +26,13 @@ struct ObjString {
   char* chars;
 };
 
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
 #endif
+
